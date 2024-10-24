2b2eab670a45d039e2f8792a89e53ef065b92df6
alibaba0010 <zakariyahali100@gmail.com>
2024-10-24 20:47:37 +0100

    Rock paper scissors smart contract

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import type { ReactNode } from "react";
+
+import { ReactQueryProvider } from "@/components/ReactQueryProvider";
+import { WalletProvider } from "@/components/WalletProvider";
+import { Toaster } from "@/components/ui/toaster";
+import { WrongNetworkAlert } from "@/components/WrongNetworkAlert";
+import { TopBanner } from "@/components/TopBanner";
+
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "NextJS Boilerplate Template",
+  description: "NextJS Boilerplate Template is a...",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <WalletProvider>
+          <ReactQueryProvider>
+            <div id="root">{children}</div>
+            <WrongNetworkAlert />
+            <TopBanner />
+            <Toaster />
+          </ReactQueryProvider>
+        </WalletProvider>
+      </body>
+    </html>
+  );
+}
